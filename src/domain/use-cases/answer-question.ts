151f58aa630775f2df76257df379cf3fa4b976ac
Pia <algauer.henrique@gmail.com>
2024-12-20 11:37:28 -0300

    implementado Unique Entity ID

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswerRespository } from "../repositories/answer-repository"
 
@@ -12,12 +13,14 @@ export class AnswerQuestionUseCase {
         private answerRepository: AnswerRespository,
     ) { }
 
-    execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const answer = new Answer({
+    async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+        const answer = Answer.create({
             content,
-            authorId: instructorId,
-            questionId,
+            authorId: new UniqueEntityID(instructorId),
+            questionId: new UniqueEntityID(questionId),
         })
+
+        await this.answerRepository.create(answer)
 
         return answer
     }
