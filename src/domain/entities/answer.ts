7fc86064869bbdca1a335ba8f5a807fc862dc6da
Pia <algauer.henrique@gmail.com>
2024-12-20 11:29:17 -0300

    atualizado getters e setters

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
     authorId: UniqueEntityID
@@ -12,5 +13,39 @@ interface AnswerProps {
 export class Answer extends Entity<AnswerProps> {
     get content() {
         return this.props.content
+    }
+    get authorId() {
+        return this.props.authorId
+    }
+    get questionId() {
+        return this.props.questionId
+    }
+    get createdAt() {
+        return this.props.createdAt
+    }
+    get updateAt() {
+        return this.props.updateAt
+    }
+
+    get excerpt() {
+        return this.content.substring(0, 120).trimEnd().concat('...')
+    }
+
+    private touch() {
+        this.props.updateAt = new Date()
+    }
+
+    set content(content: string) {
+        this.props.content = content
+        this.touch()
+    }
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date()
+        }, id)
+
+        return answer
     }
 }
