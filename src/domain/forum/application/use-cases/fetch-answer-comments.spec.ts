266460d06c9e3fe6753e8e3abefecb8ece618c94
Pia <algauer.henrique@gmail.com>
2024-12-22 21:48:20 -0300

    atualizado arquivo de teste

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -1,28 +1,55 @@
-import { AnswerComment } from "../../enterprise/entities/answer-comment"
-import { AnswerCommentsRepository } from "../repositories/answer-comments-repository"
+import { makeAnswerComment } from "../../../../../test/factories/make-answer-comment";
+import { InMemoryAnswerCommentsRepository } from "../../../../../test/repositories/in-memory-answer-comments-repository";
+import { UniqueEntityID } from "../../../../core/entities/unique-entity-id";
+import { FetchAnswerCommentsUseCase } from "./fetch-answer-comments";
 
-interface FetchAnswerCommentsUseCaseRequest {
-    answerId: string
-    page: number
-}
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
 
-interface FetchAnswerCommentsUseCaseResponse {
-    answerComments: AnswerComment[]
-}
+describe('Find a Answer comments', () => {
+    beforeEach(() => {
+        inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+        sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
+    })
+    it('should be able to fetch answer comments', async () => {
+        await inMemoryAnswerCommentsRepository.create(
+            makeAnswerComment({
+                answerId: new UniqueEntityID('answer-1')
+            })
+        )
+        await inMemoryAnswerCommentsRepository.create(
+            makeAnswerComment({
+                answerId: new UniqueEntityID('answer-1')
+            })
+        )
+        await inMemoryAnswerCommentsRepository.create(
+            makeAnswerComment({
+                answerId: new UniqueEntityID('answer-1')
+            })
+        )
 
-export class FetchAnswerCommentsUseCase {
-    constructor(
-        private answerCommentsRepository: AnswerCommentsRepository,
-    ) { }
+        const { answerComments } = await sut.execute({
+            answerId: 'answer-1',
+            page: 1
+        })
 
-    async execute({
-        answerId,
-        page
-    }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
-        const answerComments = await this.answerCommentsRepository.findManyByAnswerId(answerId, { page })
+        expect(answerComments).toHaveLength(3)
+    })
 
-        return {
-            answerComments
+    it('should be able to fetch paginated answer comments', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswerCommentsRepository.create(
+                makeAnswerComment({
+                    answerId: new UniqueEntityID('answer-1')
+                })
+            )
         }
-    }
-}
+
+        const { answerComments } = await sut.execute({
+            answerId: 'answer-1',
+            page: 2
+        })
+
+        expect(answerComments).toHaveLength(2)
+    })
+})
