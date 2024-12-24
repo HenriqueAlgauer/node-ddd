2ca2c34d26d59b5e856e1141cfa61b4a3139a980
Pia <algauer.henrique@gmail.com>
2024-12-24 13:17:12 -0300

    adicionado teste de verificação de autor da pergunta

--- src/domain/notification/application/use-cases/read-notification.spec.ts
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -1,5 +1,7 @@
 import { makeNotification } from "../../../../../test/factories/make-notification";
 import { InMemoryNotificationsRepository } from "../../../../../test/repositories/in-memory-notifications-repository";
+import { UniqueEntityID } from "../../../../core/entities/unique-entity-id";
+import { NotAllowedError } from "../../../../core/errors/errors/not-allowed-error";
 import { ReadNotificationUseCase } from "./read-notification";
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
@@ -24,5 +26,21 @@ describe('Read Notification', () => {
         expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
             expect.any(Date)
         )
+    })
+
+    it('should not be able to delete a notification from another user', async () => {
+        const newNotification = makeNotification({
+            recipientId: new UniqueEntityID('recipient-1')
+        })
+
+        await inMemoryNotificationsRepository.create(newNotification)
+
+        const result = await sut.execute({
+            recipientId: 'recipient-2',
+            notificationId: newNotification.id.toString()
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
     })
 })
